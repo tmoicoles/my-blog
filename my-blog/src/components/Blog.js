0ffa3fad455015b0270298d2398370e36c5acd32
tmoicoles <tmoi.cm@cdpipelinedevshop.com>
2022-07-21 16:31:55 +0000

    first commit

--- my-blog/src/components/Blog.js
+++ my-blog/src/components/Blog.js
@@ -14,10 +14,10 @@ import Sidebar from './Sidebar';
 import Footer from './Footer';
 
 const sections = [
-  { title: 'Technology', url: '#' },
-  { title: 'Design', url: '#' },
+  { title: 'Fear Of God', url: '#' },
   { title: 'Culture', url: '#' },
-  { title: 'Business', url: '#' },
+  { title: 'Essentials', url: '#' },
+  { title: 'Other Accesrioes', url: '#' },
   { title: 'Politics', url: '#' },
   { title: 'Opinion', url: '#' },
   { title: 'Science', url: '#' },
@@ -27,29 +27,29 @@ const sections = [
 ];
 
 const mainFeaturedPost = {
-  title: 'Title of a longer featured blog post',
+  title: 'Fear Of God Era 2022',
   description:
-    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-  image: 'https://source.unsplash.com/random',
+    "",
+  image: '',
   imageText: 'main image description',
   linkText: 'Continue reading…',
 };
 
 const featuredPosts = [
   {
-    title: 'Featured post',
-    date: 'Nov 12',
+    title: 'Fear Of God',
+    date: 'June 15',
     description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random',
+      '.',
+    image: '',
     imageLabel: 'Image Text',
   },
   {
-    title: 'Post title',
-    date: 'Nov 11',
+    title: 'Essentials',
+    date: 'June 17',
     description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random',
+      '.',
+    image: '',
     imageLabel: 'Image Text',
   },
 ];
@@ -59,19 +59,9 @@ const featuredPosts = [
 const sidebar = {
   title: 'About',
   description:
-    'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
+    'Essential Clothing means the minimum basic clothing that is absolutely necessary and indispensable due to the delay of luggage under the circumstances..',
   archives: [
-    { title: 'March 2020', url: '#' },
-    { title: 'February 2020', url: '#' },
-    { title: 'January 2020', url: '#' },
-    { title: 'November 1999', url: '#' },
-    { title: 'October 1999', url: '#' },
-    { title: 'September 1999', url: '#' },
-    { title: 'August 1999', url: '#' },
-    { title: 'July 1999', url: '#' },
-    { title: 'June 1999', url: '#' },
-    { title: 'May 1999', url: '#' },
-    { title: 'April 1999', url: '#' },
+   
   ],
   social: [
     { name: 'GitHub', icon: GitHubIcon },
